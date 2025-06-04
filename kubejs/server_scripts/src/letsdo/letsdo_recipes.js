fac590abf141a7e4f6d104c57691c1f7a28da581
Joel Burk <joel.burk@gmail.com>
2025-06-04 17:23:54 -0400

    0.3.3.1

--- kubejs/server_scripts/src/letsdo/letsdo_recipes.js
+++ kubejs/server_scripts/src/letsdo/letsdo_recipes.js
@@ -1,7 +1,8 @@
 ServerEvents.recipes((event) => {
   event
-    .shaped("beachparty:dried_wheat_block", ["AAA", "AAA", "AAA"], {
+    .shaped("beachparty:wet_hay_block", ["AAA", "ABA", "AAA"], {
       A: "brewery:dried_wheat",
+      B: "minecraft:water_bucket"
     })
-    .id("cozy_peas:beachparty_dried_wheat_block");
+    .id("cozy_peas:beachparty_wet_hay_block");
 });
